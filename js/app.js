1d76c5ec9267c86a5514931275c4730a2a2dcea6
AdamWier <38018787+AdamWier@users.noreply.github.com>
2019-01-06 19:47:12 +0100

    stuck on creating profile buttons, for loop issue

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 // Application
+var rawData = {};
 var app = {
   init: function() {
-    console.log('App : init');
+    let buildHTML = "";
+    $.getJSON("http://jsonplaceholder.typicode.com/users", function(data){
+    data.forEach((x, index) => {
+      buildHTML += (`<div class="entry"><span class="name"><a href="mailto:${x.email}">${x.name}</a></span>, Entreprise : ${x.company.name} <button class="full" id="user${index}">Profil</button></div>`)
+    })
+    $("#users").html(buildHTML);
+    for (var i = 0; i < data.length; i++){
+      $("#"+i).on("click", $("#profile").text(i))
+      console.log(i)
+    }
+    })
   },
 };
 
 // quand le DOM est prêt, on lance app.init
 $(app.init);
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("load").addEventListener("click", app.init);
+});
