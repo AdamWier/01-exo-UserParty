f2d73bc15a4cd4cc655f40dd1d56e8f5aff95261
AdamWier <38018787+AdamWier@users.noreply.github.com>
2019-01-06 20:12:09 +0100

    fixed and finished

--- js/app.js
+++ js/app.js
@@ -5,13 +5,16 @@ var app = {
     let buildHTML = "";
     $.getJSON("http://jsonplaceholder.typicode.com/users", function(data){
     data.forEach((x, index) => {
-      buildHTML += (`<div class="entry"><span class="name"><a href="mailto:${x.email}">${x.name}</a></span>, Entreprise : ${x.company.name} <button class="full" id="user${index}">Profil</button></div>`)
+      buildHTML += (`<div class="entry"><span class="name"><a href="mailto:${x.email}">${x.name}</a></span>, Entreprise : ${x.company.name} <button class="full" id="${index}">Profil</button></div>`)
     })
     $("#users").html(buildHTML);
-    for (var i = 0; i < data.length; i++){
-      $("#"+i).on("click", $("#profile").text(i))
-      console.log(i)
-    }
+    $(".full").each(x => $("#"+x).on("click", function(){
+      $("#profile").html(`<p><span class="label">Nom</span> : ${data[x].name} </p>
+      <p><span class="label">Entreprise</span> : ${data[x].company.name}, ${data[x].company.catchPhrase} </p>
+      <p><span class="label">Adresse</span> : ${data[x].address.street}, ${data[x].address.suite}, ${data[x].address.city}, ${data[x].address.zipcode}</p>
+      <p><span class="label">Numero</span> : ${data[x].phone} </p>
+      <p><span class="label">Website</span> : <a target="_blank" href="http://${data[x].website}">${data[x].website}</a></p>`)
+    }))
     })
   },
 };
